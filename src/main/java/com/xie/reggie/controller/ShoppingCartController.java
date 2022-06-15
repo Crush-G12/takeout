a81f23d324678bae422d7216e22a4418bdfd6613
Crush <1278372654@qq.com>
2022-06-15 09:29:42 +0800

    实现购物车功能

--- src/main/java/com/xie/reggie/controller/ShoppingCartController.java
+++ src/main/java/com/xie/reggie/controller/ShoppingCartController.java
@@ -1,16 +1,79 @@
 package com.xie.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xie.reggie.comon.BaseContext;
 import com.xie.reggie.comon.R;
+import com.xie.reggie.entity.ShoppingCart;
+import com.xie.reggie.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.tomcat.websocket.AsyncChannelWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("/shoppingCart")
 public class ShoppingCartController {
 
+    @Autowired
+    private ShoppingCartService shoppingCartService;
 
+    @PostMapping("/add")
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart, HttpSession session){
+        //添加到购物车里
+        Long userId = BaseContext.getId();
+        shoppingCart.setUserId(userId);
 
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId,userId);
+
+        //判断是菜品还是套餐
+        if(shoppingCart.getDishId() != null){
+            //菜品
+            queryWrapper.eq(ShoppingCart::getDishId,shoppingCart.getDishId());
+        }else if(shoppingCart.getSetmealId() != null){
+            //套餐
+            queryWrapper.eq(ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
+        }
+
+        ShoppingCart cart = shoppingCartService.getOne(queryWrapper);
+        if(cart != null){
+            //如果购物车已经存在该菜品，则数量加1
+            cart.setNumber(cart.getNumber() + 1);
+            shoppingCartService.updateById(cart);
+            shoppingCart = cart;
+        }else {
+            //如果不存在，则直接添加数据
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartService.save(shoppingCart);
+        }
+
+        return R.success(shoppingCart);
+    }
+
+    @GetMapping("/list")
+    public R<List<ShoppingCart>> list(){
+        //查询当前用户的购物车数据
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        Long userId = BaseContext.getId();
+        queryWrapper.eq(ShoppingCart::getUserId,userId);
+        queryWrapper.orderByDesc(ShoppingCart::getCreateTime);
+
+        List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
+        return R.success(list);
+    }
+
+    @DeleteMapping("/clean")
+    public R<String> clean(){
+        Long userId = BaseContext.getId();
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId,userId);
+        shoppingCartService.remove(queryWrapper);
+        return R.success("成功清空购物车");
+    }
 }
